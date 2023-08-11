ee3cbab5ef10db78b1b9f3d9c4293a66ea49b008
BellapuManojkumar <120239790+BellapuManojkumar@users.noreply.github.com>
2023-08-11 15:45:44 +0530

    a =int(input()) b = int(input()) D=a+b H=a-b A=a*b N=a//b U=a%b print(f"Sum:{D}") print(f"Difference:{H}") print(f"Product:{A}") print(f"Quotient:{N}") print(f"Remainder:{U}")

---
+++ Arithmetic_operations.py
@@ -0,0 +1,12 @@
+a =int(input())
+b = int(input())
+D=a+b
+H=a-b
+A=a*b
+N=a//b
+U=a%b
+print(f"Sum:{D}")
+print(f"Difference:{H}")
+print(f"Product:{A}")
+print(f"Quotient:{N}")
+print(f"Remainder:{U}")
