99c794108b56d633d069cae6eb26d264b23f0edf
BellapuManojkumar <120239790+BellapuManojkumar@users.noreply.github.com>
2023-08-17 22:47:14 +0530

    a=int(input()) b=int(input()) add=a+b diff=a-b prd=a*b quo=a//b rem=a%b print(f"Sum:{add}") print(f"Difference:{diff}") print(f"Product:{prd}") print(f"Quotient:{quo}") print(f"Remainder:{rem}")

--- Arithmetic_operations.py
+++ Arithmetic_operations.py
@@ -1,12 +1,12 @@
-a =int(input())
-b = int(input())
-D=a+b
-H=a-b
-A=a*b
-N=a//b
-U=a%b
-print(f"Sum:{D}")
-print(f"Difference:{H}")
-print(f"Product:{A}")
-print(f"Quotient:{N}")
-print(f"Remainder:{U}")
+a=int(input())
+b=int(input())
+add=a+b
+diff=a-b
+prd=a*b
+quo=a//b
+rem=a%b
+print(f"Sum:{add}")
+print(f"Difference:{diff}")
+print(f"Product:{prd}")
+print(f"Quotient:{quo}")
+print(f"Remainder:{rem}")
